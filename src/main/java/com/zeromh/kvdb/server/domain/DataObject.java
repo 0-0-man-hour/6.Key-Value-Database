18e04a8a073b94ad23faeb95b8bebb2f6bcd336f
park-sy <meko6970@gmail.com>
2024-06-30 17:34:15 +0900

    [feat] 논리적 저장을 위한 mongo 레포지토리 작성

---
+++ src/main/java/com/zeromh/kvdb/server/domain/DataObject.java
@@ -0,0 +1,17 @@
+package com.zeromh.kvdb.server.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class DataObject {
+    @Id
+    String key;
+    Object value;
+}
---
+++ src/main/java/com/zeromh/kvdb/server/infrastructure/store/impl/MongoRepository.java
@@ -0,0 +1,35 @@
+package com.zeromh.kvdb.server.infrastructure.store.impl;
+
+import com.zeromh.consistenthash.domain.model.key.HashKey;
+import com.zeromh.kvdb.server.domain.DataObject;
+import com.zeromh.kvdb.server.infrastructure.store.StorePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+
+@Repository
+@RequiredArgsConstructor
+public class MongoRepository implements StorePort {
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+    private static final String REPLICA = "R_";
+
+    @Override
+    public Mono<DataObject> getValue(HashKey key, boolean isReplica) {
+        String collectionName = (isReplica?REPLICA:"")+key.getServerHash();
+        return reactiveMongoTemplate.findById(key.getKey(), DataObject.class, collectionName);
+    }
+
+    @Override
+    public Mono<DataObject> saveValue(HashKey key, DataObject dataObject, boolean isReplica) {
+        String collectionName = (isReplica?REPLICA:"")+key.getServerHash();
+        Query query = new Query(Criteria.where("_id").is(dataObject.getKey()));
+        Update update = new Update().set("value", dataObject.getValue());
+        return reactiveMongoTemplate.upsert(query, update, collectionName)
+                .thenReturn(dataObject);
+    }
+}
